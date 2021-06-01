3912320611d83dd53cd68d3487f0a5ed6957de7e
idrecun <idrecun@gmail.com>
2021-06-01 10:35:03 +0200

    Fix VarMap ordering

--- Main.hs
+++ Main.hs
@@ -1,6 +1,8 @@
 import Logic
 import ParseLogic
 import Tseytin
+import Data.List
+import Data.Ord
 import Data.Map
 import Data.Set
 import System.IO
@@ -9,8 +11,12 @@ type VarMap = Map String Int
 
 distinct list = Data.Set.toList (Data.Set.fromList list)
 
+varComparator :: String -> (String, Int)
+varComparator ('$' : t) = ("$", read t)
+varComparator s = (s, 0)
+
 getVarList :: LogicCNF -> [String]
-getVarList formula = distinct (Prelude.map varname (concat formula))
+getVarList formula = sortBy (comparing varComparator) (distinct (Prelude.map varname (concat formula)))
 
 getVarMap :: LogicCNF -> VarMap
 getVarMap formula = Data.Map.fromList (zip varlist [1..length varlist])
@@ -21,7 +27,7 @@ literalString m (Pure v) = show (m ! v) ++ " "
 literalString m (Neg v)  = "-" ++ show (m ! v) ++ " "
 
 dimacs :: Logic -> LogicCNF -> String
-dimacs formula cnf = "c " ++ (show $ formula) ++ "\nc " ++ show (Data.Map.toList varmap) ++ "\np cnf " ++ show (length varmap) ++ " " ++ show (length cnf) ++ "\n" ++ clauses
+dimacs formula cnf = "c " ++ (show $ formula) ++ "\nc " ++ show (sortBy (comparing snd) (Data.Map.toList varmap)) ++ "\np cnf " ++ show (length varmap) ++ " " ++ show (length cnf) ++ "\n" ++ clauses
   where varmap = getVarMap cnf
         clauses = concat (Prelude.map ((++ "0\n") . concat . (Prelude.map (literalString varmap))) cnf)
 
